3e476c1da38d515e3515414888511a9bbb6e1fbd
Fferalv <fferalv454@iesalmudeyne.es>
2024-10-16 08:35:50 +0200

    Version inicial sin los colores

---
+++ Branching.java
@@ -0,0 +1,8 @@
+package GestionRamas;
+
+public class Branching {
+    public static void main (String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo de nuevo");
+    }
+}
