430632fabb965fe726c292f12501cf1444c5b283
Fferalv <fferalv454@iesalmudeyne.es>
2024-10-16 09:51:39 +0200

    Colores arreglados

--- Branching.java
+++ Branching.java
@@ -2,7 +2,7 @@ package GestionRamas;
 
 public class Branching {
     public static void main (String[] args) {
-        String rojo = "\033[32m";
+        String rojo = "\033[31m";
         String azul = "\033[34m";
         System.out.println(rojo + "Hola mundo");
         System.out.println(azul + "Aquí estamos haciendo prueba");
