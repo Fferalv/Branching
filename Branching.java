ef5b83daab691c29cc4e556d2546db6c26544f92
Fferalv <fferalv454@iesalmudeyne.es>
2024-10-16 08:39:25 +0200

    Version del fichero color colores

--- Branching.java
+++ Branching.java
@@ -2,7 +2,9 @@ package GestionRamas;
 
 public class Branching {
     public static void main (String[] args) {
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("Hola mundo");
-        System.out.println("Hola mundo de nuevo");
+        System.out.println("Aquí estamos haciendo prueba");
     }
 }
