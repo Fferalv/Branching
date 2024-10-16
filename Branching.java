6ffb24b6f1de053940ec4a012388a0b517703f62
Fferalv <fferalv454@iesalmudeyne.es>
2024-10-16 09:03:20 +0200

    Saludo en colores

--- Branching.java
+++ Branching.java
@@ -4,7 +4,7 @@ public class Branching {
     public static void main (String[] args) {
         String rojo = "\033[32m";
         String azul = "\033[34m";
-        System.out.println("Hola mundo");
-        System.out.println("Aquí estamos haciendo prueba");
+        System.out.println(rojo + "Hola mundo");
+        System.out.println(azul + "Aquí estamos haciendo prueba");
     }
 }
